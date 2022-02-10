3abc09be270da6dc92e21a12bdb6f4de7082909f
Kyle Chen <NeoNeuron@users.noreply.github.com>
2022-02-11 01:10:23 +0800

    Update scripts for flight_test.py.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Slides for central limit theorem
+
+```bash
+conda install -y jupyter notebook ipython numpy matplotlib scipy ffmpeg
+pip install svgpathtools svgpath2mpl
+```
--- flight_test.py
+++ flight_test.py
@@ -4,14 +4,29 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from matplotlib.animation import FuncAnimation
-from scipy.stats import binom, bernoulli, boltzmann
+from scipy.stats import bernoulli
 from svgpathtools import svg2paths
 from svgpath2mpl import parse_path
+# matplotlib parameters to ensure correctness of Chinese characters 
+plt.rcParams["font.family"] = 'sans-serif'
+plt.rcParams['font.sans-serif']=['Arial Unicode MS', 'SimHei'] # Chinese font
+plt.rcParams['axes.unicode_minus']=False # correct minus sign
+
 plt.rcParams["font.size"] = 20
 plt.rcParams["xtick.labelsize"] = 24
 plt.rcParams["ytick.labelsize"] = 24
 
-def gen_marker(fname, rotation=180):
+def gen_marker(fname:str, rotation:float=180):
+    """Generate maker from svg image file.
+
+    Args:
+        fname (str): filename of svg image.
+        rotation (int, optional): 
+            degree of rotation of original images. Defaults to 180.
+
+    Returns:
+        Object of marker.
+    """
     person_path, attributes = svg2paths(fname)
     person_marker = parse_path(attributes[0]['d'])
     person_marker.vertices -= person_marker.vertices.mean(axis=0)
@@ -22,52 +37,73 @@ def gen_marker(fname, rotation=180):
 flight_marker = gen_marker('flight.svg',0)
 #%%
 # test marker
-xx, yy = np.meshgrid(np.arange(10), np.arange(10))
-fig, ax = plt.subplots(1,1, figsize=(10,10), dpi=150)
+xx, yy = np.meshgrid(np.arange(4), np.arange(4))
+fig, ax = plt.subplots(1,1, figsize=(5,5))
 ax.scatter(xx.flatten(),yy.flatten(),  s=2000, color='k', marker=flight_marker )
 ax.axis('off')
 #%%
+class UpdateFigure:
+    def __init__(self, 
+        ax:plt.Axes, data:np.ndarray, ax_main:plt.Axes, 
+        ax_right:plt.Axes, ax_top:plt.Axes, n_days:int):
+        """Plot the first frame for the animation.
 
-class UpdateDist:
-    def __init__(self, ax, data, ax_main, ax_right, ax_top, days):
+        Args:
+            ax (plt.Axes): axes of flight icons.
+            data (np.ndarray): 1-D array of number of passagers for each days
+            ax_main (plt.Axes): axes of scatter plot
+            ax_right (plt.Axes): axes of histogram
+            ax_top (plt.Axes): axes of line plot
+            n_days (int): number of days to plot
+        """
+
+        self.colors = dict(
+            flight_init=[0,0,0,1],
+            main=np.array([0,109,135,255])/255.0, #006D87
+            gauss=np.array([177,90,67,255])/255.0, #B15A43
+            flight_red=np.array([230,0,18,255])/255.0,
+            flight_green=np.array([0,176,80,255])/255.0,
+        )
         self.ax = ax
+        # generate the grid of flights
         xn, yn = 37, 10
         xx, yy = np.meshgrid(np.arange(xn), np.arange(yn))
-        self.sc_flight = self.ax.scatter(xx.flatten()[:days],yy.flatten()[:days], s=2000, facecolor=[0,0,0,1], marker=flight_marker)
-        self.color = np.tile([0,0,0,1],(int(days),1)).astype(float)
+        self.sc_flight = self.ax.scatter(
+            xx.flatten()[:n_days], yy.flatten()[:n_days], s=2000, 
+            facecolor=self.colors['flight_init'], marker=flight_marker)
+        # array to record the color of each flight
+        self.color = np.tile(self.colors['flight_init'],(int(n_days),1)).astype(float)
         self.ax.set_xlim(-1,xn)
         self.ax.set_ylim(-1,yn)
         self.ax.invert_yaxis()
         self.data = data
-        self.day_data = np.arange(data.shape[0])+1
+        self.days = np.arange(data.shape[0])+1
 
-        # main plot:
+        # scatter plot:
         self.line_main, = ax_main.plot([], [], 'o',
-            color='#006D87',
+            color=self.colors['main'],
             markersize=6,
             markerfacecolor='none',
             markeredgewidth=2)
         ax_main.set_xlabel('日期', fontsize=40)
         ax_main.set_ylabel('登机人数', fontsize=40)
-        # ax_main.grid(linestyle='--')
 
         # now determine nice limits by hand:
         ymax = np.max(np.fabs(data))
         ymin = np.min(np.fabs(data))
         xlim = (-1, data.shape[0]+1)
         ylim = (int(ymin*0.95), int(ymax*1.05))
-
         ax_main.set_xlim(xlim)
         ax_main.set_ylim(ylim)
-
         self.ax_main = ax_main
 
+        # initialize the bins of histogram
         self.bins = int(ylim[1]-ylim[0] + 1)
         self.range = [ylim[0]-0.5, ylim[1]+0.5]
         counts, edges = np.histogram(data, range=self.range, bins = self.bins)
-        self.rects = ax_right.barh((edges[1:]+edges[:-1])/2, np.zeros_like(counts), color='#006D87')
+        self.rects = ax_right.barh((edges[1:]+edges[:-1])/2, np.zeros_like(counts), color=self.colors['main'])
         self.data_line_top = (np.cumsum(data<=300)/(np.arange(data.shape[0])+1))*100
-        self.line_top, = ax_top.plot([],[], color='#006D87', lw=5)
+        self.line_top, = ax_top.plot([],[], color=self.colors['main'], lw=5)
         ax_top.set_ylabel('乘客均有座概率(%)', fontsize=40)
         ax_right.set_xlabel('天数', fontsize=40)
 
@@ -82,8 +118,8 @@ class UpdateDist:
         y_grid = np.linspace(ylim[0], ylim[1], 200)
         gauss_curve = func(y_grid, 0.9*seats, 0.09*seats)
         gauss_curve /= np.sum(gauss_curve*(y_grid[1]-y_grid[0]))
-        gauss_curve *= days
-        ax_right.plot(gauss_curve, y_grid, color='#B15A43', lw=5)
+        gauss_curve *= n_days
+        ax_right.plot(gauss_curve, y_grid, color=self.colors['gaussian'], lw=5)
 
         self.ax_top= ax_top
         self.ax_right= ax_right
@@ -93,20 +129,20 @@ class UpdateDist:
         # watching new realizations of the process
 
         if i > 0:
-            n_inc = 5
+            n_inc = 1
             # update lines
             idx = (i)*n_inc
-            self.line_main.set_data(self.day_data[:idx], self.data[:idx])
-            self.line_top.set_data(self.day_data[:idx], self.data_line_top[:idx])
+            self.line_main.set_data(self.days[:idx], self.data[:idx])
+            self.line_top.set_data(self.days[:idx], self.data_line_top[:idx])
             
+            # update the height of bars for histogram
             counts, edges = np.histogram(self.data[:idx], range=self.range, bins = self.bins)
             for rect, h in zip(self.rects, counts):
                 rect.set_width(h)
             # update scatter facecolor
             for j in np.arange(n_inc):
-                # idx = i-1
                 idx = (i-1)*n_inc+j
-                self.color[idx,:] = [230./255,0./255,18./255,1] if self.data[idx]>300 else [0,176./255,80./255,1]
+                self.color[idx,:] = self.colors['flight_red'] if self.data[idx]>300 else self.colors['flight_green']
             self.sc_flight.set_facecolor(self.color)
         return self.rects
 # %%
@@ -125,9 +161,6 @@ attendence = my_distribution.rvs(**my_dist_args, size=(K,n), random_state=99239)
 days = 365
 seats = 319
 margin = np.sum(attendence[100:100+days,:seats], axis=1, dtype=float)
-
-from matplotlib.ticker import NullFormatter 
-nullfmt = NullFormatter()         # no labels
 
 fig = plt.figure(figsize=(30,20),dpi=100)
 spec1 = gridspec.GridSpec(ncols=1, nrows=1, left=0.04, right=0.96, top=0.98, bottom=0.60, figure=fig)
@@ -148,20 +181,15 @@ axHistx = plt.axes(rect_histx)
 axHisty = plt.axes(rect_histy)
 
 # no labels
+from matplotlib.ticker import NullFormatter 
+nullfmt = NullFormatter()         # no labels
 axHistx.xaxis.set_major_formatter(nullfmt)
 axHisty.yaxis.set_major_formatter(nullfmt)
 
-ud = UpdateDist(ax0, margin, axMain, axHisty, axHistx, days)
-anim = FuncAnimation(fig, ud, frames=74, interval=150, blit=True)
-anim.save('flight_movie.mp4', dpi=100, codec='mpeg4')
-# %%
-from moviepy.editor import *
-
-fname = "flight_movie.mp4"
-video = VideoFileClip(fname, audio=False)
-video = video.subclip(0,video.duration)
-
-video.to_videofile(fname.split('.')[0]+'_recompressed.mp4', fps=24)
-
-
+# create a figure updater
+ud = UpdateFigure(ax0, margin, axMain, axHisty, axHistx, days)
+# user FuncAnimation to generate frames of animation
+anim = FuncAnimation(fig, ud, frames=366, blit=True)
+# save animation as *.mp4
+anim.save('flight_movie.mp4', fps=24, dpi=100, codec='libx264', bitrate=-1, extra_args=['-pix_fmt', 'yuv420p'])
 # %%
