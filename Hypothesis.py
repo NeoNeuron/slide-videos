203cb517a72cf71957c92e2794e390ff698b8f8e
Kyle Chen <NeoNeuron@users.noreply.github.com>
2022-02-26 23:36:33 +0800

    Update code for hypothesis.

--- Hypothesis.py
+++ Hypothesis.py
@@ -1,7 +1,7 @@
 # %%
-from functools import partial
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import norm
 from matplotlib.animation import FuncAnimation
 # matplotlib parameters to ensure correctness of Chinese characters 
 plt.rcParams["font.family"] = 'sans-serif'
@@ -27,25 +27,29 @@ class UpdateFigure:
         self.ax.spines["left"].set_visible(False)
         self.dx = dx
         self.mean = 7.0
-        self.sigma = 0.05
+        self.sigma = 0.04
         self.th = 0.0784
-        self.x = np.linspace(-0.25,0.25,500) + self.mean
+        self.x = np.linspace(-0.25,0.25,1000) + self.mean
 
         self.ax.set_xlim(self.x[0],self.x[-1])
-        self.ax.set_ylim(0, 6.0)
-        self.gauss = partial(self._gauss, mean=self.mean, sigma=self.sigma)
+        self.ax.set_ylim(0, 20.0)
+        self.norm = norm(loc=self.mean, scale=self.sigma)
+        self.gauss = self.norm.pdf
+        self.isf = self.norm.isf(0.025)
+        self.ppf = self.norm.ppf(0.025)
+
         self.line = self.ax.plot(self.x,self.gauss(self.x), color='k', zorder=1)
         self.ax.axvline(self.mean, ymax=0.95, ls='--', color='#C00000')
 
-        vline0 = self.ax.axvline(self.mean+self.th, ymax=0.7, ls='--', color='#002060')
-        vline1 = self.ax.axvline(self.mean-self.th, ymax=0.7, ls='--', color='#002060')
+        vline0 = self.ax.axvline(norm.isf(0.025, loc=self.mean, scale=self.sigma), ymax=0.7, ls='--', color='#002060')
+        vline1 = self.ax.axvline(norm.ppf(0.025, loc=self.mean, scale=self.sigma), ymax=0.7, ls='--', color='#002060')
         self.vlines = [vline0, vline1]
 
-        shade0 = self._shading(self.mean+self.th, 'right')
-        shade1 = self._shading(self.mean-self.th, 'left')
+        shade0 = self._shading(self.isf, 'right')
+        shade1 = self._shading(self.ppf, 'left')
         self.shades = [shade0, shade1]
-        stick0, text0 = self.create_text(self.mean+self.th, 'right')
-        stick1, text1 = self.create_text(self.mean-self.th, 'left')
+        stick0, text0 = self.create_text(self.isf, 'right')
+        stick1, text1 = self.create_text(self.ppf, 'left')
         self.texts = [text0, text1]
         self.sticks = [stick0, stick1]
         self.ax.set_yticks([])
@@ -56,10 +60,10 @@ class UpdateFigure:
         offset = 0.005
         if direction == 'right':
             stick, = self.ax.plot(x+np.array([offset, offset+width]),self.gauss(x+offset)/2+np.array([0., height]), color='k', zorder=1)
-            text = self.ax.text(x+offset+0.02+width, height+self.gauss(x+offset)/2, r'$\frac{\alpha}{2}$', ha='left')
+            text = self.ax.text(x+offset+0.005+width, height+self.gauss(x+offset)/2, r'$\frac{\alpha}{2}$', ha='left')
         elif direction == 'left':
             stick, = self.ax.plot(x-np.array([offset, offset+width]),self.gauss(x-offset)/2+np.array([0., height]), color='k', zorder=1)
-            text = self.ax.text(x-offset-0.02-width, height+self.gauss(x-offset)/2, r'$\frac{\alpha}{2}$', ha='right')
+            text = self.ax.text(x-offset-0.005-width, height+self.gauss(x-offset)/2, r'$\frac{\alpha}{2}$', ha='right')
         return stick, text
 
     def update_text(self, x, stick, text, direction='right'):
@@ -68,24 +72,27 @@ class UpdateFigure:
         offset = 0.005
         if direction == 'right':
             stick.set_data(x+np.array([offset, offset+width]),self.gauss(x+offset)/2+np.array([0., height]))
-            text.set_position((x+offset+0.02+width, height+self.gauss(x+offset)/2))
+            text.set_position((x+offset+0.005+width, height+self.gauss(x+offset)/2))
         elif direction == 'left':
             stick.set_data(x-np.array([offset, offset+width]),self.gauss(x-offset)/2+np.array([0., height]))
-            text.set_position((x-offset-0.02-width, height+self.gauss(x-offset)/2))
+            text.set_position((x-offset-0.005-width, height+self.gauss(x-offset)/2))
         return stick, text
 
     def update_gauss(self):
         self.line[0].set_data(self.x,self.gauss(self.x))
 
-    @staticmethod
-    def _gauss(x, mean, sigma):
-        return np.exp(-(x-mean)**2/(2*sigma**2))/2/np.pi/sigma
+    def update_vlines(self):
+        [self.vlines[i].set_data(np.ones(2)*val, [0,0.6]) for i, val in enumerate((self.isf, self.ppf))]
 
     def _shading(self, x, direction='right'):
         if direction == 'right':
-            art = self.ax.fill_between(self.x[self.x>=x], 0,  self.gauss(self.x[self.x>=x]), fc='#1F77B4', alpha=0.5)
+            art = self.ax.fill_between(self.x[self.x>=x], 0,
+                                       self.gauss(self.x[self.x>=x]),
+                                       fc='#1F77B4', alpha=0.5)
         if direction == 'left':
-            art = self.ax.fill_between(self.x[self.x<=x], 0, self.gauss(self.x[self.x<=x]), fc='#1F77B4', alpha=0.5)
+            art = self.ax.fill_between(self.x[self.x<=x], 0,
+                                       self.gauss(self.x[self.x<=x]), 
+                                       fc='#1F77B4', alpha=0.5)
         return art
 
     # def __call__(self, i):
@@ -102,16 +109,20 @@ class UpdateFigure:
     def __call__(self, i):
         # This way the plot can continuously run and we just keep
         # watching new realizations of the process
-        self.gauss = partial(self._gauss, mean=self.mean, sigma=self.sigma-i*self.dx)
+        self.norm = norm(loc=self.mean, scale=self.sigma-i*self.dx)
+        self.gauss = self.norm.pdf
+        self.isf = self.norm.isf(0.025)
+        self.ppf = self.norm.ppf(0.025)
         self.update_gauss()
+        self.update_vlines()
         # eps = 0.5+i*self.depsilon
         # [self.vlines[i].set_data(np.ones(2)*val, [0,0.6]) for i, val in enumerate((eps, -eps))]
         [shade.remove() for shade in self.shades]
-        shade0 = self._shading(self.mean+self.th, 'right')
-        shade1 = self._shading(self.mean-self.th, 'left')
+        shade0 = self._shading(self.isf, 'right')
+        shade1 = self._shading(self.ppf, 'left')
         self.shades = [shade0, shade1]
-        self.update_text(self.mean+self.th, self.sticks[0], self.texts[0], 'right')
-        self.update_text(self.mean-self.th, self.sticks[1], self.texts[1], 'left')
+        self.update_text(self.isf, self.sticks[0], self.texts[0], 'right')
+        self.update_text(self.ppf, self.sticks[1], self.texts[1], 'left')
         return self.line
 # %%
 fig, ax = plt.subplots(1,1, figsize=(10,3),dpi=400)
--- Hypothesis1.py
+++ Hypothesis1.py
@@ -1,6 +1,7 @@
 # %%
 from functools import partial
 import numpy as np
+from scipy.stats import norm
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 # matplotlib parameters to ensure correctness of Chinese characters 
@@ -27,25 +28,31 @@ class UpdateFigure:
         self.ax.spines["left"].set_visible(False)
         self.dx = dx
         self.mean = 6.8
-        self.sigma = 0.05
+        self.sigma = 0.04
         self.th = 0.0784
         self.x = np.linspace(-0.25,0.25,500) + 7.0#self.mean
 
         self.ax.set_xlim(self.x[0],self.x[-1])
-        self.ax.set_ylim(0, 9.0)
-        self.gauss = partial(self._gauss, mean=self.mean, sigma=self.sigma)
+        self.ax.set_ylim(0, 20.0)
+
+        self.norm = norm(loc=self.mean, scale=self.sigma)
+        self.norm1 = norm(loc=7, scale=self.sigma)
+        self.gauss = self.norm.pdf
+        self.isf = self.norm1.isf(0.025)
+        self.ppf = self.norm1.ppf(0.025)
+
         self.line = self.ax.plot(self.x,self.gauss(self.x), color='k', zorder=1)
         self.ax.axvline(self.mean, ymax=0.95, ls='--', color='#C00000')
 
-        vline0 = self.ax.axvline(7+self.th, ymax=0.7, ls='--', color='#002060')
-        vline1 = self.ax.axvline(7-self.th, ymax=0.7, ls='--', color='#002060')
+        vline0 = self.ax.axvline(self.isf, ymax=0.7, ls='--', color='#002060')
+        vline1 = self.ax.axvline(self.ppf, ymax=0.7, ls='--', color='#002060')
         self.vlines = [vline0, vline1]
 
-        shade0 = self._shading(7.0-self.th, 'right')
-        # shade1 = self._shading(7.0+self.th, 'left')
+        shade0 = self._shading(self.ppf, 'right')
+        # shade1 = self._shading(self.isf, 'left')
         self.shades = [shade0,]
-        stick0, text0 = self.create_text(7.0-self.th, 'right', r'$\beta$')
-        # stick1, text1 = self.create_text(7.0+self.th, 'left', r'$\beta$')
+        stick0, text0 = self.create_text(self.ppf, 'right', r'$\beta$')
+        # stick1, text1 = self.create_text(self.isf, 'left', r'$\beta$')
         self.texts = [text0,]
         self.sticks = [stick0,]
         self.ax.set_yticks([])
@@ -56,10 +63,10 @@ class UpdateFigure:
         offset = 0.010
         if direction == 'right':
             stick, = self.ax.plot(x+np.array([offset, offset+width]),self.gauss(x+offset)/2+np.array([0., height]), color='k', zorder=1)
-            text = self.ax.text(x+offset+0.02+width, height+self.gauss(x+offset)/2, text, ha='left')
+            text = self.ax.text(x+offset+0.005+width, height+self.gauss(x+offset)/2, text, ha='left')
         elif direction == 'left':
             stick, = self.ax.plot(x-np.array([offset, offset+width]),self.gauss(x-offset)/2+np.array([0., height]), color='k', zorder=1)
-            text = self.ax.text(x-offset-0.02-width, height+self.gauss(x-offset)/2, text, ha='right')
+            text = self.ax.text(x-offset-0.005-width, height+self.gauss(x-offset)/2, text, ha='right')
         return stick, text
 
     def update_text(self, x, stick, text, direction='right'):
@@ -68,18 +75,17 @@ class UpdateFigure:
         offset = 0.010
         if direction == 'right':
             stick.set_data(x+np.array([offset, offset+width]),self.gauss(x+offset)/2+np.array([0., height]))
-            text.set_position((x+offset+0.02+width, height+self.gauss(x+offset)/2))
+            text.set_position((x+offset+0.005+width, height+self.gauss(x+offset)/2))
         elif direction == 'left':
             stick.set_data(x-np.array([offset, offset+width]),self.gauss(x-offset)/2+np.array([0., height]))
-            text.set_position((x-offset-0.02-width, height+self.gauss(x-offset)/2))
+            text.set_position((x-offset-0.005-width, height+self.gauss(x-offset)/2))
         return stick, text
 
     def update_gauss(self):
         self.line[0].set_data(self.x,self.gauss(self.x))
 
-    @staticmethod
-    def _gauss(x, mean, sigma):
-        return np.exp(-(x-mean)**2/(2*sigma**2))/2/np.pi/sigma
+    def update_vlines(self):
+        [self.vlines[i].set_data(np.ones(2)*val, [0,0.6]) for i, val in enumerate((self.isf, self.ppf))]
 
     def _shading(self, x, direction='right'):
         if direction == 'right':
@@ -102,16 +108,21 @@ class UpdateFigure:
     def __call__(self, i):
         # This way the plot can continuously run and we just keep
         # watching new realizations of the process
-        self.gauss = partial(self._gauss, mean=self.mean, sigma=self.sigma-i*self.dx)
+        self.norm = norm(loc=self.mean, scale=self.sigma-i*self.dx)
+        self.norm1 = norm(loc=7.0, scale=self.sigma-i*self.dx)
+        self.gauss = self.norm.pdf
+        self.isf = self.norm1.isf(0.025)
+        self.ppf = self.norm1.ppf(0.025)
         self.update_gauss()
+        self.update_vlines()
         # eps = 0.5+i*self.depsilon
         # [self.vlines[i].set_data(np.ones(2)*val, [0,0.6]) for i, val in enumerate((eps, -eps))]
         [shade.remove() for shade in self.shades]
-        shade0 = self._shading(7-self.th, 'right')
-        # shade1 = self._shading(7+self.th, 'left')
+        shade0 = self._shading(self.ppf, 'right')
+        # shade1 = self._shading(self.isf, 'left')
         self.shades = [shade0,]
-        self.update_text(7.0-self.th, self.sticks[0], self.texts[0], 'right')
-        # self.update_text(7+self.th, self.sticks[0], self.texts[0], 'left')
+        self.update_text(self.ppf, self.sticks[0], self.texts[0], 'right')
+        # self.update_text(self.isf, self.sticks[0], self.texts[0], 'left')
         return self.line
 # %%
 fig, ax = plt.subplots(1,1, figsize=(10,3),dpi=400)
