d8b517ee16f4e6af69261a57a03d4f66092f6a55
Kyle Chen <NeoNeuron@users.noreply.github.com>
2022-02-26 23:36:53 +0800

    Fix the fps issue for windows media player.

--- discretize_func.py
+++ discretize_func.py
@@ -1,6 +1,7 @@
 # %%
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import norm
 from matplotlib.animation import FuncAnimation
 # matplotlib parameters to ensure correctness of Chinese characters 
 plt.rcParams["font.family"] = 'sans-serif'
@@ -12,10 +13,7 @@ plt.rcParams["xtick.labelsize"] = 24
 plt.rcParams["ytick.labelsize"] = 24
 plt.rcParams["axes.spines.top"] = False
 plt.rcParams["axes.spines.right"] = False
-#%%
-def gauss(x):
-    return np.exp(-(x-1)**2/2)/2/np.pi
-
+plt.rcParams["axes.spines.left"] = False
 #%%
 class UpdateFigure:
     def __init__(self, 
@@ -36,7 +34,8 @@ class UpdateFigure:
         )
         self.ax = ax
         self.ax.set_xlim(-2,4)
-        self.ax.set_ylim(0,0.2)
+        self.ax.set_ylim(0,0.5)
+        self.ax.set_yticks([])
         self.data = data
         self.line=self.ax.plot([],[])
 
@@ -48,22 +47,25 @@ class UpdateFigure:
             # update lines
             self.ax.cla()
             self.ax.set_xlim(-2,4)
-            self.ax.set_ylim(0,0.2)
-            for xval, yval in zip(np.linspace(-2,4, self.data[i]), gauss(np.linspace(-2,4,self.data[i]))):
-                self.ax.axvline(xval,ymax=yval/0.2, ls='--', lw=1, color='gray')
-                self.ax.plot(xval, yval, 'o', color=plt.cm.rainbow(1-(xval+2)/6))
+            self.ax.set_ylim(0,0.5)
+            self.ax.set_yticks([])
+            for xval, yval in zip(np.linspace(-2,4, self.data[i]), norm.pdf(np.linspace(-2,4,self.data[i]),1,1)):
+                self.ax.axvline(xval,ymax=yval/0.5, ls='--', lw=1, color='gray')
+                line, = self.ax.plot(xval, yval, 'o', color=plt.cm.rainbow(1-(xval+2)/6), ms=10)
+                line.set_clip_on(False)
         elif i == self.data.shape[0]:
             self.ax.cla()
             self.ax.set_xlim(-2,4)
-            self.ax.set_ylim(0,0.2)
+            self.ax.set_ylim(0,0.5)
+            self.ax.set_yticks([])
             N = 1000
-            xvals, yvals = np.linspace(-2,4, N), gauss(np.linspace(-2,4,N))
+            xvals, yvals = np.linspace(-2,4, N), norm.pdf(np.linspace(-2,4,N),1,1)
             for i in range(N-1):
-                self.ax.plot(xvals[i:i+2], yvals[i:i+2], color=plt.cm.rainbow(1-(xvals[i]+2)/6), lw=4)
-
+                line, = self.ax.plot(xvals[i:i+2], yvals[i:i+2], color=plt.cm.rainbow(1-(xvals[i]+2)/6), lw=4)
+                line.set_clip_on(False)
         return self.line
 # %%
-fig, ax = plt.subplots(1,1, figsize=(10,8),dpi=200)
+fig, ax = plt.subplots(1,1, figsize=(8,6),dpi=400)
 
 n_segments = 2**np.arange(1,8, dtype=int)
 # create a figure updater
@@ -71,5 +73,14 @@ ud = UpdateFigure(ax, n_segments)
 # user FuncAnimation to generate frames of animation
 anim = FuncAnimation(fig, ud, frames=8, blit=True)
 # save animation as *.mp4
-anim.save('discrete_func.mp4', fps=1, dpi=200, codec='libx264', bitrate=-1, extra_args=['-pix_fmt', 'yuv420p'])
+anim.save('discrete_func.mp4', fps=1, dpi=400, codec='libx264', bitrate=-1, extra_args=['-pix_fmt', 'yuv420p'])
+# %%
+# reprocess the video to fix the imcompatible issue for windows user
+from moviepy.editor import *
+
+fname = "discrete_func.mp4"
+video = VideoFileClip(fname, audio=False)
+video = video.subclip(0,video.duration)
+
+video.to_videofile(fname.split('.')[0]+'_cliped.mp4',fps=24)
 # %%
