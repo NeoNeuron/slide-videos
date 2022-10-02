e0de30bcfd8b4e7a30c48aa2576cf968ffdbf11c
Kyle Chen <NeoNeuron@users.noreply.github.com>
2022-10-02 10:39:47 +0800

    modify med_test

--- med_test.py
+++ med_test.py
@@ -11,21 +11,16 @@ plt.rcParams["xtick.labelsize"] = 25
 plt.rcParams["ytick.labelsize"] = 25
 
 #%%
-np.random.seed(1901)
+np.random.seed(1900)
 xx, yy = np.meshgrid(np.arange(100), np.arange(100))
 indices = np.random.choice(np.arange(10000), 100, replace=False)
 mask = np.zeros(10000).astype(bool)
 mask[indices] = True
-# fig, ax = plt.subplots(1,1, figsize=(10,10), dpi=150)
-# ax.scatter(xx.flatten()[mask],yy.flatten()[mask],  s=40, color='k', marker='x' )
-# ax.scatter(xx.flatten()[~mask],yy.flatten()[~mask],s=20, color='k', )
-# ax.set_xlim(-1,100)
-# ax.set_ylim(-1,100)
 # %%
 rvar = np.random.rand(mask.shape[0])
 test_res = np.zeros_like(mask)
 for i in range(mask.shape[0]):
-    test_res[i] = rvar[i] >= 0.10 if mask[i] else rvar[i] >= 0.91
+    test_res[i] = rvar[i] >= 0.10 if mask[i] else rvar[i] >= 0.99
 
 #%%
 
@@ -42,8 +37,6 @@ patient_marker = gen_marker('icons/patient.svg')
 
 class UpdateDist:
     def __init__(self, ax0, ax, ax1, patient_mask):
-        self.success = 0
-
         self.ax0 = ax0
         xn, yn = 40, 10
         xx, yy = np.meshgrid(np.arange(xn), np.arange(yn))
@@ -65,52 +58,43 @@ class UpdateDist:
         self.ax.set_yticks([1,2,3])
         self.ax.set_yticklabels(["","", ""])
         self.ax.set_xlabel("检测人数", fontsize=60)
-        # self.ax.set_yticklabels(["检测阳性新冠患者","检测阳性健康人群", "检测阴性"], fontsize=20)
         self.ax.text(-0.160, 0.15, "新冠患者", transform=self.ax.transAxes, fontsize=30, color='r', )
         self.ax.text(-0.160, 0.26, "检测阳性", transform=self.ax.transAxes, fontsize=30, color=[228./255,131./255,18./255,1], )
         self.ax.text(-0.160, 0.41, "健康人群", transform=self.ax.transAxes, fontsize=30, color=[0,32./255,96./255,1], )
         self.ax.text(-0.160, 0.51, "检测阳性", transform=self.ax.transAxes, fontsize=30, color=[228./255,131./255,18./255,1], )
         self.ax.text(-0.160, 0.71, "检测阴性", transform=self.ax.transAxes, fontsize=30, color=[0,176./255,80./255,1], )
-        # self.ax.set_yticklabels(["True Positive","False Positive", "Negative"], fontsize=20)
-        # self.ax.set_xlabel("Number of people", fontsize=20)
 
         # Set up plot parameters
         self.ax.set_ylim(0, 4)
-        self.ax.set_xlim(0, 10000)
+        self.ax.set_xlim(0, 4)
+        self.ax.set_xticks(np.arange(5))
+        self.ax.set_xticklabels([r'$10^{%d}$'%i for i in np.arange(5)], fontsize=40)
         self.ax.spines['top'].set_visible(False)
         self.ax.spines['right'].set_visible(False)
 
         self.line, =ax1.plot([],[])
         self.ax1 = ax1
         self.ax1.set_xlim([0,10000])
-        self.ax1.set_ylim([0,0.2])
-        self.ax1.set_yticks([0,0.1,0.2])
+        self.ax1.set_ylim([0,1.0])
+        self.ax1.set_xticks(np.arange(6)*2000)
+        self.ax1.set_xticklabels(['%d'%i for i in np.arange(6)*2000], fontsize=40)
+        self.ax1.set_yticks([0,0.5,1.0])
         self.ax1.set_ylabel("检测准确率", fontsize=50)
         self.ax1.set_xlabel("检测人数", fontsize=60)
-        # self.ax1.set_xlabel('Number of people tested', fontsize=20)
-        # self.ax1.set_ylabel('Test accuracy', fontsize=20)
         self.ax1.spines['top'].set_visible(False)
         self.ax1.spines['right'].set_visible(False)
-        self.ax1.axhline(0.0917, linestyle='--', color='black')
+        self.ax1.axhline(0.476, linestyle='--', color='black')
+        self.ax1.text(0.85, 0.52, '0.476', color='black', transform=self.ax1.transAxes, fontsize=50)
 
-        # This vertical line represents the theoretical value, to
-        # which the plotted distribution should converge.
 
     def __call__(self, i):
         # This way the plot can continuously run and we just keep
         # watching new realizations of the process
         if i == 0:
-            self.success = 0
             for rect, h in zip(self.rects, [0,0]): 
                 rect.set_width(h)
             self.line, = self.ax1.plot([], [], lw=5, color='r')
             return self.rects
-
-        # Choose success based on exceed a threshold with a uniform pick
-        # if np.random.rand(1,) < self.prob:
-        #     self.success += 1
-        # y = beta_pdf(self.x, self.success + 1, (i - self.success) + 1)
-        # self.line.set_data(self.x, y)
 
         if i <= 100:
             n_inc = 4
@@ -119,7 +103,7 @@ class UpdateDist:
             healthy_n = (test_res*(~mask))[:n_inc*(i+1)].sum()
             negative = np.sum(~test_res[:n_inc*(i+1)])
             for rect, h in zip(self.rects, [sick_n,healthy_n, negative]): 
-                rect.set_width(h)
+                rect.set_width(np.log10(h+1))
             # update curve
             xdata, ydata = self.line.get_data()
             if len(xdata) == 0:
@@ -136,7 +120,7 @@ class UpdateDist:
             healthy_n = (test_res*(~mask))[:401+n_inc*(i-99)].sum()
             negative = np.sum(~test_res[:401+n_inc*(i-99)])
             for rect, h in zip(self.rects, [sick_n,healthy_n, negative]): 
-                rect.set_width(h)
+                rect.set_width(np.log10(h+1))
             # update curve
             xdata, ydata = self.line.get_data()
             xdata = np.append(xdata, xdata[-1]+n_inc) 
@@ -159,10 +143,10 @@ fig = plt.figure(figsize=(30,17),dpi=100)
 spec1 = gridspec.GridSpec(ncols=1, nrows=1, left=0.04, right=0.96, top=0.98, bottom=0.38, figure=fig)
 ax0 = fig.add_subplot(spec1[0])
 ax0.axis('off')
-spec2 = gridspec.GridSpec(ncols=2, nrows=1, left=0.08, right=0.92, top=0.32, bottom=0.08, wspace=0.20, figure=fig)
+spec2 = gridspec.GridSpec(ncols=2, nrows=1, left=0.08, right=0.96, top=0.35, bottom=0.11, wspace=0.20, figure=fig)
 ax1 = fig.add_subplot(spec2[0])
 ax2 = fig.add_subplot(spec2[1])
 ud = UpdateDist(ax0, ax1, ax2, mask)
-anim = FuncAnimation(fig, ud, frames=198, blit=True)
+anim = FuncAnimation(fig, ud, frames=204, blit=True)
 anim.save('covid.mp4', fps=12, dpi=100, codec='libx264', bitrate=-1, extra_args=['-pix_fmt', 'yuv420p'])
 # %%
