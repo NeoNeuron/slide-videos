3499e273d05e987a57c020a284601d013b347e68
Kyle Chen <NeoNeuron@users.noreply.github.com>
2022-11-10 23:48:02 +0800

    update scripts of linear_regression

--- lr_examples.py
+++ lr_examples.py
@@ -1,7 +1,7 @@
 #%%
-PATH = 'LinearRegression/'
-import os
-os.makedirs(PATH, exist_ok=True)
+from pathlib import Path
+PATH = Path('LinearRegression/')
+PATH.mkdir(exist_ok=True)
 import numpy as np
 import matplotlib.pyplot as plt
 plt.rcParams['font.size'] = 20
@@ -26,12 +26,12 @@ y=1.1*x-20.5 + np.random.randn(x.shape[0])*2
 fig, ax = create_canvas()
 ax.plot(x, y, 'o', ms=15, markerfacecolor='royalblue', markeredgecolor='orange')
 ax.set_xlabel('父母平均身高(cm)', fontsize=30)
-ax.set_ylabel('子女身高(cm)', fontsize=30)
+ax.set_ylabel('女孩成年后身高(cm)', fontsize=30)
 ax.set_ylim(151, 171)
-fig.savefig(PATH+'parents_offspring_height1.png', dpi=200)
+fig.savefig(PATH/'parents_offspring_height1.png', dpi=200)
 p = np.polyfit(x, y, 1)
 line, = ax.plot([x.min(),x.max()], np.polyval(p, [x.min(), x.max()]), 'r')
-fig.savefig(PATH+'parents_offspring_height2.png', dpi=200)
+fig.savefig(PATH/'parents_offspring_height2.png', dpi=200)
 x_range = np.array([156, 210])
 line.set_data(x_range, 5.01+0.95*x_range)
 line.set_color('royalblue')
@@ -40,7 +40,7 @@ ax.set_ylim(151, 205)
 ax.scatter([208],[203],marker='^',s=140,c='r', zorder=10)
 ax.plot([208, 208],[148, 203],'--',c='r', zorder=10)[0].set_clip_on(False)
 ax.plot([153, 208],[203, 203],'--',c='r', zorder=10)[0].set_clip_on(False)
-fig.savefig(PATH+'parents_offspring_height3.png', dpi=200)
+fig.savefig(PATH/'parents_offspring_height3.png', dpi=200)
 
 # %%
 np.random.seed(509)
@@ -50,10 +50,10 @@ ax.plot(x, y, 'o', ms=15, markerfacecolor='royalblue', markeredgecolor='orange')
 ax.set_xlabel('身高(cm)', fontsize=30)
 ax.set_ylabel('体重(kg)', fontsize=30)
 ax.set_ylim(54, 74)
-fig.savefig(PATH+'height_weight1.png', dpi=200)
+fig.savefig(PATH/'height_weight1.png', dpi=200)
 p = np.polyfit(x, y, 1)
 ax.plot([x.min(),x.max()], np.polyval(p, [x.min(), x.max()]), 'r')
-fig.savefig(PATH+'height_weight2.png', dpi=200)
+fig.savefig(PATH/'height_weight2.png', dpi=200)
 
 # %%
 
@@ -65,8 +65,8 @@ ax.plot(x, y, 'o', ms=15, markerfacecolor='royalblue', markeredgecolor='orange')
 ax.set_xlabel('住房面积($m^2$)', fontsize=30)
 ax.set_ylabel('房价(万)', fontsize=30)
 ax.set_ylim(230, 680)
-fig.savefig(PATH+'house_price1.png', dpi=200)
+fig.savefig(PATH/'house_price1.png', dpi=200)
 p = np.polyfit(x, y, 1)
 ax.plot([x.min(),x.max()], np.polyval(p, [x.min(), x.max()]), 'r')
-fig.savefig(PATH+'house_price2.png', dpi=200)
+fig.savefig(PATH/'house_price2.png', dpi=200)
 #%%
--- lr_height_CI.py
+++ lr_height_CI.py
@@ -1,7 +1,7 @@
 # %%
-PATH = 'LinearRegression/'
-import os
-os.makedirs(PATH, exist_ok=True)
+from pathlib import Path
+PATH = Path('LinearRegression/')
+PATH.mkdir(exist_ok=True)
 import numpy as np
 import matplotlib.pyplot as plt
 # matplotlib parameters to ensure correctness of Chinese characters 
@@ -31,7 +31,7 @@ fig, ax = plt.subplots(1,1,figsize=(8,6),
                        gridspec_kw = dict(left=0.15, right=0.88, top=0.9, bottom=0.15)
                        )
 ax.set_xlabel('父母平均身高(cm)', fontsize=30)
-ax.set_ylabel('女孩身高(cm)', fontsize=30)
+ax.set_ylabel('女孩成年后身高(cm)', fontsize=30)
 ax.set_ylim([150,220])
 ax.set_xlim([155,210]) 
 ax.set_xticks([155+i*10 for i in range(6)])
@@ -47,11 +47,11 @@ X = np.linspace(156,208,100)
 Y = np.zeros([100])
 for i in range(100):
 	Y[i] = get_width(X[i])
-fig.savefig(PATH+'lr_height_1.png', dpi=300)
+fig.savefig(PATH/'lr_height_1.png', dpi=300)
 b0 = 5.01
 b1 = 0.95
 ax.plot([156,208], b1*np.array([156,208])+b0, c='mediumblue')
-fig.savefig(PATH+'lr_height_2.png', dpi=300)
+fig.savefig(PATH/'lr_height_2.png', dpi=300)
 
 def CI(x, b0, b1):
 	width = get_width(x)
@@ -59,17 +59,17 @@ def CI(x, b0, b1):
 	l2 = ax.axhline(b1*x+b0-width, xmax=(x-155)/(210-155), ls='--', color='r')
 	lines=ax.errorbar([x],[b1*x+b0], yerr=width, capsize=8, marker='^',ms=10,c='r', alpha=0.8, zorder=10)
 	ax.text(x+1,b1*x+b0, f'({x:.0f}, {b1*x+b0:.0f})', fontsize=15, ha='left', va='top')
-	text1 = ax.text(156,b1*x+b0+width+0.3, f'{b1*x+b0+width:.0f}', fontsize=12, ha='left', va='bottom', color='r', alpha=0.8)
-	text2 = ax.text(156,b1*x+b0-width-0.4, f'{b1*x+b0-width:.0f}', fontsize=12, ha='left', va='top', color='r', alpha=0.8)
+	text1 = ax.text(156,b1*x+b0+width+0.3, f'{np.ceil(b1*x+b0+width):.0f}', fontsize=12, ha='left', va='bottom', color='r', alpha=0.8)
+	text2 = ax.text(156,b1*x+b0-width-0.4, f'{np.ceil(b1*x+b0-width):.0f}', fontsize=12, ha='left', va='top', color='r', alpha=0.8)
 	return [l1, l2, lines.lines[1][0], lines.lines[1][1], lines.lines[2][0], text1, text2]
 
 lines =CI(208, b0, b1)
-fig.savefig(PATH+'lr_height_3.png', dpi=300)
+fig.savefig(PATH/'lr_height_3.png', dpi=300)
 [line.set_color('w') for line in lines]
 lines =CI(165, b0, b1)
-fig.savefig(PATH+'lr_height_4.png', dpi=300)
+fig.savefig(PATH/'lr_height_4.png', dpi=300)
 [line.set_color('w') for line in lines]
 ax.plot(X, b1*X+b0+Y, ls='--',color = '#1f77b4')
 ax.plot(X, b1*X+b0-Y, ls='--',color = '#1f77b4')
-fig.savefig(PATH+'lr_height_5.png', dpi=300)
+fig.savefig(PATH/'lr_height_5.png', dpi=300)
 # %%
