6e07c36051644ceb3cfdbab1f5f47c6f19b45552
Kyle Chen <NeoNeuron@users.noreply.github.com>
2023-02-14 16:45:43 +0800

    update intervel estimation

--- intervel_estimation.py
+++ intervel_estimation.py
@@ -19,40 +19,21 @@ plt.rcParams["font.size"] = 20
 plt.rcParams["xtick.labelsize"] = 30
 plt.rcParams["ytick.labelsize"] = 30
 
-# %%
-#数据
+# %% data
 np.random.seed(1901)
 mean,std=605,10 #均值，标准差
-size=2000
-data1=np.random.normal(mean,std,int(size))
-#mean1,std1=600,8
-#data1_new=np.random.normal(mean1,std1,int(size*0.2))
-#print(data1.shape,data1_new.shape)
-#data1=np.concatenate((data1,data1_new))
-#np.random.shuffle(data1)
+size=(200, 10)
+data=np.random.randn(*size)*std+mean
+data_norm=(data-data.min())/(data.max()-data.min())
 
-data1_norm=(data1-min(data1))/(max(data1)-min(data1))
-
-mask = np.zeros(10000).astype(bool)
-data=data1.reshape(-1,10)
-data_=data1.reshape(-1,50)
-
-data_mean=np.sum(data,axis=1)/10
-#data_mean=data_mean[:200]
-#data_mean1=np.sum(data_,axis=1)/50
+data_mean = np.mean(data, axis=1)
 index=np.arange(1,1001,1)
-
-data_mean.shape
-
 # %%
 length=10**0.5*1.96
-
-# %%
 test=np.zeros_like(data_mean)
 for i in range(len(data_mean)):
     test[i]= data_mean[i]-length<=mean<=data_mean[i]+length
 test=test.astype('bool')
-
 
 # %%
 test_num=test.astype('int')
@@ -81,18 +62,15 @@ car_marker = gen_marker('icons/car.svg',)
 
 # %%
 class UpdateDist:
-    def __init__(self, ax0, ax_main,ax_colorbar,ax_bottom1,ax_bottom2,patient_mask):
+    def __init__(self, ax0, ax_main,ax_colorbar,ax_bottom1,ax_bottom2):
         self.success = 0
 
         self.ax0 = ax0
         xn, yn = 10, 1
         xx, yy = np.meshgrid(np.arange(xn), np.arange(yn))
-        # indices = np.random.choice(np.arange(int(xn*yn)), int(xn*yn/100), replace=False)
-        self.mask_plt = patient_mask[:int(xn*yn)]
-        #self.sc_patient = ax0.scatter(xx.flatten()[self.mask_plt],yy.flatten()[self.mask_plt],  s=2000, facecolor=[1,0,0,1], marker=patient_marker)
-        self.sc_car  = ax0.scatter(xx.flatten()[~self.mask_plt],yy.flatten()[~self.mask_plt],s=10000,  facecolor=[1,1,1,1], edgecolor=[0,0,0,1], marker=car_marker)
+        self.sc_car  = ax0.scatter(xx.flatten(),yy.flatten(),s=10000, 
+                                   fc='w', ec='k', marker=car_marker)
         self.color = np.tile([0,32./255,96./255,1],(int(xn*yn),1))
-        #self.color[self.mask_plt,:] = [1,0,0,1]
         self.ax0.set_xlim(-1,xn)
         self.ax0.set_ylim(-1,yn)
         self.ax0.invert_yaxis()
@@ -100,7 +78,6 @@ class UpdateDist:
         # main plot:
         self.line_main1, = ax_main.plot([], [], '--',lw=2,color='k')
         self.line_main2, = ax_main.plot([], [], '--',lw=2,color='k')
-        #ax_main.plot([mean,mean],[-0.5,0.5],linestyle='--',color='r',lw=5) 
         ax_main.axvline(x=mean,linestyle='--',color='r',lw=5)
         ax_main.text(mean+0.3,-0.1,r'$\mu$',fontsize=50, va='top', ha='left')
         
@@ -125,22 +102,15 @@ class UpdateDist:
         self.bottom1.set_yticks([1,2])
         self.bottom1.set_yticklabels(["", ""])
         self.bottom1.set_xlabel("置信区间数", fontsize=40)
-        # self.ax.set_yticklabels(["检测阳性新冠患者","检测阳性健康人群", "检测阴性"], fontsize=20)
-        # self.ax.text(-0.130, 0.17, "新冠患者", transform=self.ax.transAxes, fontsize=25, color='r', )
-        # self.ax.text(-0.130, 0.27, "检测阳性", transform=self.ax.transAxes, fontsize=25, color=[228./255,131./255,18./255,1], )
-        # self.ax.text(-0.130, 0.43, "健康人群", transform=self.ax.transAxes, fontsize=25, color=[0,32./255,96./255,1], )
         self.bottom1.text(-21, 1+0.18, r'$\mu$在', fontsize=30, va='center', ha='center', color=[0,176./255,80./255,1], )
         self.bottom1.text(-21, 1-0.18, "置信区间内", fontsize=30, va='center', ha='center', color=[0,176./255,80./255,1], )
         self.bottom1.text(-21, 2+0.18, r"$\mu$不在", fontsize=30, va='center', ha='center', color=[228./255,131./255,18./255,1], )
         self.bottom1.text(-21, 2-0.18, "置信区间内", fontsize=30, va='center', ha='center', color=[228./255,131./255,18./255,1], )
-        # self.ax.set_yticklabels(["True Positive","False Positive", "Negative"], fontsize=20)
-        # self.ax.set_xlabel("Number of people", fontsize=20)
         # Set up plot parameters
         self.bottom1.set_ylim(0.4, 2.6)
         self.bottom1.set_xlim(0, 200)
         self.bottom1.spines['top'].set_visible(False)
         self.bottom1.spines['right'].set_visible(False)
-        
         
         #bottom plot2
         self.line2,=ax_bottom2.plot([],[],linestyle='-',color='black',lw=5,zorder=0)
@@ -157,14 +127,13 @@ class UpdateDist:
         ax_bottom2.set_ylim(ylim1)
         self.ax_bottom2 = ax_bottom2
         
-        
         #draw colorbar
         self.cm = plt.cm.RdYlBu_r
         gradient = np.atleast_2d(np.linspace(0, 1, 256))
         ax_colorbar.imshow(gradient, aspect='auto', cmap=self.cm, alpha=0.7)
         ax_colorbar.set_yticks([])
         ax_colorbar.set_xticks([0, 255])
-        ax_colorbar.set_xticklabels(['低', '高'])
+        ax_colorbar.set_xticklabels(['560', '630'])
         ax_colorbar.set_title('里程数', fontsize=30)
 
 
@@ -172,7 +141,9 @@ class UpdateDist:
         # This way the plot can continuously run and we just keep
         # watching new realizations of the process
 
-        if i > 0:
+        if i <= data_mean.shape[0]:
+            if i == 0:
+                i = 20 # frontpage
             n_inc = 1
             # update lines
             idx = (i)*n_inc
@@ -180,7 +151,6 @@ class UpdateDist:
             self.line_main1.set_data([data_mean[i]-length,data_mean[i]-length],[-0.5,0.5])
             self.line_main2.set_data([data_mean[i]+length,data_mean[i]+length],[-0.5,0.5])
             self.shadow.set_verts([[[data_mean[i]-length,-0.5],[data_mean[i]-length,0.5],[data_mean[i]+length,0.5],[data_mean[i]+length,-0.5]]])
-            #self.ax_main.fill_between([data_mean[i]-length,data_mean[i]+length],-0.5 ,0.5, fc='b', alpha=0.3)
             #更新直方图
             negative = np.sum(~test[:i+1]) #未索赔车辆数
             positive = np.sum(test[:i+1]) #索赔车辆数
@@ -189,13 +159,9 @@ class UpdateDist:
             #更新频率图
             self.line2.set_data(index[:idx], frequency[:idx])
             
-            
             # update scatter facecolor
-            for j in range(10):
-                indx=10*i+j
-                self.color[j,:] = self.cm(data1_norm[indx])
+            self.color = self.cm(data_norm[idx])
             self.sc_car.set_facecolor(self.color)
-            
             
         return self.rects
     
@@ -213,8 +179,9 @@ ax4=fig.add_subplot(spec4[0])
 spec2 = gridspec.GridSpec(ncols=2, nrows=1, left=0.08, right=0.95, top=0.45, bottom=0.1, wspace=0.25, figure=fig)
 ax1 = fig.add_subplot(spec2[0])
 ax3 = fig.add_subplot(spec2[1])
-ud = UpdateDist(ax0, ax4, ax2,ax1,ax3,mask)
+ud = UpdateDist(ax0, ax4, ax2,ax1,ax3)
 plt.savefig(path/'test.pdf')
+#%%
 anim = FuncAnimation(fig, ud, frames=198, blit=True)
 anim.save(path/'mileagejie_2.mp4', fps=10, dpi=100, codec='libx264', bitrate=-1, extra_args=['-pix_fmt', 'yuv420p'])
 # %%
