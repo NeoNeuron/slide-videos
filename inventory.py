c639f3aa526f6e1d9c68064cdb6d4d4762eede5d
Kai Chen(zhoucpu) <kchen513@outlook.com>
2023-03-07 11:43:55 +0800

    update poisson

--- inventory.py
+++ inventory.py
@@ -19,25 +19,26 @@ class UpdateDist:
         self.xdata = np.arange(1, data.shape[0]+1)
 
         # main plot:
-        self.line_main, = ax.plot([], [], lw=5, 
-            c='#65E8F5', clip_on=False, zorder=5, label=r'$\overline{X}$')
+        self.line_main, = ax.plot([], [], lw=3, 
+            c='#C00000', clip_on=False, zorder=5, label=r'$\overline{X}$')
         self.dots, = ax.plot([], [], 'o',
             ms=8, mew=1.5, 
-            mec='#353A71', mfc='#A7BED2', clip_on=True, zorder=1, label=r'$X_n$')
+            # mec='#C00000', mfc='#D77F66', clip_on=True, zorder=1, label=r'$X_n$')
+            mec='#353A71', mfc='#A7BED2', clip_on=True, zorder=1, label=r'$X_\mathrm{n}$')
         ax.set_xlabel('随机次数(n)', fontsize=24)
-        ax.set_ylabel(r'$X_n$', fontsize=24)
+        ax.set_ylabel(r'$X_\mathrm{n}$', fontsize=24)
         ax.set_title(f'n = {0:<5d}    '+r'$\overline{X}$'+f' = {0:<5.2f}', fontsize=20)
-        ax.legend(loc=1)
+        ax.legend(loc=1, framealpha=0.9)
 
         # now determine nice limits by hand:
         ax.set_xlim((-1, 101))
         ax.set_ylim(np.array([0.1,lam*2.]))
         ax.set_yticks([lam, lam*2])
         self.ax = ax
-        self.line = self.ax.axhline(lam, ls='--', color='#C00000', zorder=0)
+        self.line = self.ax.axhline(lam, ls='--', color='k', zorder=0)
         text = self.ax.text(0.07, 0.75, r'$\lambda$=%d'%lam, 
-                     transform=self.ax.transAxes, fontsize=30, color='#C00000', zorder=20)
-        text.set_bbox(dict(facecolor=[1,1,1,0.7], edgecolor='#C00000'))
+                     transform=self.ax.transAxes, fontsize=30, color='k', zorder=20)
+        text.set_bbox(dict(facecolor=[1,1,1,0.7], edgecolor='none'))
 
     def __call__(self, i):
         if i > 0:
@@ -47,15 +48,17 @@ class UpdateDist:
             self.ax.set_title(f'n = {idx:<5d}    '+r'$\mathrm{\overline{X}}$'+f'= {self.cummean[idx-1]:<5.2f}', fontsize=20)
         return [self.line]
 
-lam=10
-n = 100 # number of accumulated samples
-flip_results = poisson.rvs(mu=lam, size=(n,), random_state=8)
-fig, ax1 = plt.subplots(1,1, figsize=(8,4), gridspec_kw={'bottom':0.2})
+def gen_movie(lam):
+    n = 100 # number of accumulated samples
+    flip_results = poisson.rvs(mu=lam, size=(n,), random_state=8)
+    fig, ax = plt.subplots(1,1, figsize=(8,4), gridspec_kw={'bottom':0.2})
 
-n_in_each_frame = (np.linspace(1,10,47)**2).astype(int)
-n_in_each_frame = np.hstack(([0], n_in_each_frame))
-ud = UpdateDist(flip_results, lam, ax1, n_in_each_frame)
-anim = FuncAnimation(fig, ud, frames=n_in_each_frame.shape[0], blit=True)
-fname = path/f"inventory_{lam:d}.mp4"
-anim.save(fname, fps=12, dpi=400, codec='libx264', bitrate=-1, extra_args=['-pix_fmt', 'yuv420p'])
+    n_in_each_frame = (np.linspace(1,10,47)**2).astype(int)
+    n_in_each_frame = np.hstack(([0], n_in_each_frame))
+    ud = UpdateDist(flip_results, lam, ax, n_in_each_frame)
+    anim = FuncAnimation(fig, ud, frames=n_in_each_frame.shape[0], blit=True)
+    anim.save(path/f"inventory_{lam:d}.mp4", fps=12, dpi=400, codec='libx264', bitrate=-1, extra_args=['-pix_fmt', 'yuv420p'])
+
+gen_movie(5)
+gen_movie(10)
 # %%
--- inventory_optim.py
+++ inventory_optim.py
@@ -5,6 +5,8 @@ path.mkdir(exist_ok=True)
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import poisson
+plt.rcParams["xtick.labelsize"] = 16
+plt.rcParams["ytick.labelsize"] = 16
 
 def _poisson(lam, x):
     s = np.zeros(x.shape)
@@ -18,28 +20,21 @@ Y = np.zeros([n])
 X = np.arange(n)
 poi = _poisson(lam, X)
 kpk = np.arange(n) * poi
-print(poi)
 for z in range(n):
 	Y[z] = 1100 * np.sum(kpk[:(z+1)]) - 1100 * z * np.sum(poi[:(z+1)]) + 1000 *z
 
-print(Y)
-print(np.argmax(Y))# 14
 fig, ax = plt.subplots(1,1, figsize=(8,5),dpi=150)
 ax.set_ylim(0,1e4)
 ax.set_xlim(-1,100)
-ax.set_xlabel('库存大小(z)', fontsize=30)
-ax.set_ylabel('平均利润(Y)', fontsize=30)
+ax.set_xlabel('库存大小(z)', fontsize=25)
+ax.set_ylabel('平均利润($Y$)', fontsize=25)
 
 ax.axvline(14, ymax=0.94, ls='--', color='#C00000')
 
 ax.plot(X, Y)
 ax.plot(14,Y[14],'o',color='r')
-ax.text(50, 7400, r'平均销量 $\lambda$=10', fontsize=30)
-plt.xticks(fontsize= 15)
-plt.yticks(fontsize= 15)
-plt.annotate(r'$z^*$=14',xy=(14,Y[14]), 
-             xytext=(16,Y[14]), ha='left', va='top',
-             fontsize=30)
+ax.text(50, 7400, r'平均销量 $\lambda$=10', fontsize=25)
+ax.text(16,1200,r'$z^*$=14',ha='left', va='top', fontsize=25)
 fig.savefig(path/'inventory_optim.pdf', bbox_inches='tight')
 
 # %%
